30abbdfb6f5962930d568f1f5b92371053bcbfdd
Zach Retterath <ztretterath@gmail.com>
2016-09-12 17:53:59 -0400

    Add event listener to game tiles

--- connectFour.js
+++ connectFour.js
@@ -1,10 +1,19 @@
 $(function () {
   var gameTile = function() {
-    for (var i = 0; i < 43; i++) {
+    for (var i = 0; i < 42; i++) {
       var tile = $("<div class='gameTile'></div>")
       $('.board').append(tile);
     }
-  } //end gameTile
-
+  }
   gameTile();
+  //create 7*6 gameboard
+
+  $('.gameTile').on('click', function() {
+    console.log('I work');
+    //test
+
+    $(this).addClass('clickedWhite');
+  })
+  //change color of gameTile
+
 }) //end onload
