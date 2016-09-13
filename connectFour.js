58c80bccccb8ca9725ba6144423493fadba05f91
Zach Retterath <ztretterath@gmail.com>
2016-09-13 11:06:27 -0400

    Check for four reds in a row

--- connectFour.js
+++ connectFour.js
@@ -2,15 +2,28 @@ $(function () {
   var gameTile = function() {
     for (var i = 0; i < 42; i++) {
       var tile = $("<div class='gameTile'></div>");
-      tile.attr('index', i); //for future methods
+      tile.attr('id', i); //for future methods
       $('.board').append(tile);
     }
   }
   gameTile();
   //create 7*6 gameboard
 
-  var clicked = true;
+  var checkStraightRed = function() {
+    for (var i = 0; i < 42; i + 7) {
+      var inARow = 0;
+      var gameTile = $('#i') //iterate ids
+      if (gameTile.attr('class') === 'clickedRed') {
+        inARow ++; //add up to 4
+        if (inARow === 4) {
+          console.log('you win!');
+        } //end inARow if
+      } //end class check if
+    } //end for loop
+  } //end checkStraightRed method
 
+  //click for game pieces
+  var clicked = true;
   $('.gameTile').on('click', function() {
     if (clicked === true) {
       clicked = false;
