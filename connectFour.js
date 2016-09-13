a688550e18a5b220f2445659fa8de5e06543a93a
Zach Retterath <ztretterath@gmail.com>
2016-09-13 14:56:48 -0400

    Begin check rows for win method

--- connectFour.js
+++ connectFour.js
@@ -1,75 +1,40 @@
 $(function () {
+
   var gameTile = function() {
     for (var i = 0; i < 42; i++) {
-      var tile = $('<obj>i</obj>');
+      var tile = $("<div class='gameTile'></div>");
+      tile.attr('id', i); //for future methods
       $('.board').append(tile);
     }
   }
   gameTile();
-
-  // var gameBoard = {
-  //   ['', '', '', '', '', '', ''],
-  //   ['', '', '', '', '', '', ''],
-  //   ['', '', '', '', '', '', ''],
-  //   ['', '', '', '', '', '', ''],
-  //   ['', '', '', '', '', '', ''],
-  //   ['', '', '', '', '', '', '']
-  // }
   //create 7*6 gameboard
 
-  var makeRows = function() {
-    var rowOne = {};
-    var rowTwo = {};
-    var rowThree = {};
-    var rowFour = {};
-    var rowFive = {};
-    var rowSix = {};
+  var checkRow = function() {
+    for (var i = 0; i < 42; i++) {
+      var redCount = 0;
+      var checkId = $(this).attr('id');
+      if (this.class() === 'clickedRed') {
 
-    for (var i = 0; i <= 42; i++) {
-      var tile = $("obj");
-      if (tile.attr('#id') <= 6) {
-        rowOne.id = i;
-      } else if (tile.attr('#id') <= 13) {
-        rowTwo.id = i;
-      } else if (tile.attr('#id') <= 20) {
-        rowThree.id = i;
-      } else if (tile.attr('#id') <= 27) {
-        rowFour.id = i;
-      } else if (tile.attr('#id') <= 34) {
-        rowFive.id = i;
-      } else if (tile.attr('#id') <= 42) {
-        rowSix.id = i;
       }
     }
-    console.log(rowOne);
-    console.log(rowTwo);
-
-  } //end makeRows
-  makeRows();
-
-  var checkRow = function() {
-
   } //end checkRow method
 
   var clicked = true;
-  $('obj').on('click', function() {
+  $('.gameTile').on('click', function() {
     if (clicked === true) {
-      clicked = false;
+      clicked = false; //change player colors
       $(this).addClass('clickedRed');
-
-      console.log('now red');
-        //test
+      console.log('now red'); //test
     } else if (clicked === false) {
-      clicked = true;
+      clicked = true; //change player colors
       $(this).addClass('clickedBlack');
-
-      console.log('now black');
-        //test
+      console.log('now black'); //test
     }
   })
 
   $('#reset').on('click', function() {
-    $('obj').removeClass('clickedRed clickedBlack');
+    $('.gameTile').removeClass('clickedRed clickedBlack');
   })
 
 
